847771488ea156f6e4bf9a6dbbffcde892f9d651
shijiev <2424782003@qq.com>
2023-08-07 23:02:42 +0800

    Initial commit

---
+++ src/main/java/io/shijiev/demo/SpringBootDemosApplication.java
@@ -0,0 +1,13 @@
+package io.shijiev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemosApplication.class, args);
+    }
+
+}
